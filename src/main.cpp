ecb47a56d9be70aac8b83aabe9972c0fc10ec0d4
MaximKanevskiy <maxkanevskiy430@yandex.ru>
2025-03-23 10:46:31 +0300

    initial commit

---
+++ src/main.cpp
@@ -0,0 +1,65 @@
+﻿#include <iostream>
+#include <crow.h>
+#include <format>
+#include <chrono>
+#include <random>
+#include <crow/middlewares/session.h>
+#include <crow/middlewares/cookie_parser.h>
+
+using Session = crow::SessionMiddleware<crow::FileStore>;
+
+// returns timestamp of first user entrance
+std::string generateCookie()
+{
+	auto now = std::chrono::current_zone()->to_local(std::chrono::system_clock::now());
+	std::random_device dev;
+	std::mt19937 rng(dev());
+	std::uniform_int_distribution<int> dist(1, 10000);
+	return std::format("date: {:%x} time: {:%T} --- {}", now, now, dist(rng));
+}
+
+std::string readFile(const std::string& path) 
+{
+	std::ifstream file(path, std::ios::binary);
+	if (!file) return "";
+	return std::string((std::istreambuf_iterator<char>(file)), std::istreambuf_iterator<char>());
+}
+
+int main(int argc, char** argv)
+{
+	crow::App<crow::CookieParser, Session> app{ Session{
+		crow::FileStore{"tmp/sessiondata"}
+	}};
+
+	CROW_ROUTE(app, "/")([&](crow::request request) {
+		if (request.method == crow::HTTPMethod::Get)
+		{
+			std::cout << "Get\n";
+		}
+
+		auto& ctx = app.get_context<crow::CookieParser>(request);
+
+		if (ctx.get_cookie("key") == "")
+		{
+			ctx.set_cookie("key", generateCookie())
+				.path("/")
+				.max_age(120);
+		}		
+		
+		std::string mainPage = crow::mustache::load_text("index.html");
+		mainPage.append(ctx.get_cookie("key"));
+		return mainPage;
+	});
+
+	CROW_ROUTE(app, "/static/<string>")([](const std::string& filename) {
+
+		std::string script = readFile("static/" + filename);
+		crow::response response(script);
+		response.set_header("Content-Type", "application/javascript");
+		return response;
+	});
+
+	app.port(5559).multithreaded().run();
+
+	return 0;
+}
---
+++ static/script.js
@@ -0,0 +1,3 @@
+document.getElementById("test_button").addEventListener("click", () => {
+	alert("Hello, World gthgtuhg!");
+});
